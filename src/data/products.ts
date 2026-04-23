aa316cd9e64cb16a071f2c1c2945bc9d24c1c841
akbar-widya <akbarwidya.dev@gmail.com>
2026-04-23 10:15:10 +0700

    edit product images

--- src/data/products.ts
+++ src/data/products.ts
@@ -3,44 +3,42 @@ import type { Product } from '../lib/types';
 export const products: Product[] = [
   {
     id: '1',
-    name: 'Tas Rajut Mini',
-    note: 'Tersedia berbagai warna pastel. Cocok untuk gaya kasual sehari-hari.',
+    name: 'Slingbag Kecil',
+    note: 'Tempat bedak & handphone. Tersedia warna pastel. Cocok untuk gaya kasual sehari-hari.',
     image:
-      'https://images.pexels.com/photos/1152077/pexels-photo-1152077.jpeg?auto=compress&cs=tinysrgb&w=600',
+      '/assets/sling-bag-mini.jpeg',
   },
-  {
-    id: '2',
-    name: 'Pouch Rajut',
-    note: 'Ukuran sedang, muat dompet & HP. Ideal untuk simpan barang bawaan.',
-    image:
-      'https://images.pexels.com/photos/5632399/pexels-photo-5632399.jpeg?auto=compress&cs=tinysrgb&w=600',
-  },
+//   {
+//     id: '2',
+//     name: 'Pouch Rajut',
+//     note: 'Ukuran sedang, muat dompet & HP. Ideal untuk simpan barang bawaan.',
+//     image:
+//       'https://images.pexels.com/photos/5632399/pexels-photo-5632399.jpeg?auto=compress&cs=tinysrgb&w=600',
+//   },
   {
     id: '3',
-    name: 'Gantungan Kunci Rajut',
+    name: 'Gantungan Kunci Cupcake',
     note: 'Bentuk lucu beragam pilihan. Hadiah sempurna untuk orang tersayang.',
     image:
-      'https://images.pexels.com/photos/3692750/pexels-photo-3692750.jpeg?auto=compress&cs=tinysrgb&w=600',
+      '/assets/gantungan-kunci.jpg',
   },
   {
     id: '4',
-    name: 'Keranjang Rajut',
-    note: 'Ukuran besar, kuat menahan beban. Serbaguna untuk dekorasi rumah.',
+    name: 'Keset Corak',
+    note: 'Awet, bahan menyerap air. Serbaguna untuk dekorasi rumah.',
     image:
-      'https://images.pexels.com/photos/4464482/pexels-photo-4464482.jpeg?auto=compress&cs=tinysrgb&w=600',
+      '/assets/keset-corak.jpeg',
   },
   {
     id: '5',
     name: 'Dompet Rajut',
     note: 'Desain simpel dan elegan. Bahan benang pilihan yang tahan lama.',
-    image:
-      'https://images.pexels.com/photos/934063/pexels-photo-934063.jpeg?auto=compress&cs=tinysrgb&w=600',
+    image: '/assets/dompet-koin.jpeg',
   },
   {
     id: '6',
-    name: 'Tote Bag Rajut',
+    name: 'Slingbag Etnik Besar',
     note: 'Kapasitas besar, ringan dibawa. Tersedia berbagai motif pilihan.',
-    image:
-      'https://images.pexels.com/photos/1152077/pexels-photo-1152077.jpeg?auto=compress&cs=tinysrgb&w=600',
+    image:'/assets/sling-bag-medium.jpeg',
   },
 ];
